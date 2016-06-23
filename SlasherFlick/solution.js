99e935b3896d0c82f20818803d8d4d2422e5417f
vatsalpande <vatspande@gmail.com>
2016-06-22 23:18:39 -0700

    Slasher Flick

---
+++ SlasherFlick/solution.js
@@ -0,0 +1,8 @@
+(function () {
+    "use strict";
+    function slasher(arr, howMany) {
+        // it doesn't always pay to be first
+        return arr.splice(howMany, arr.length);
+    }
+    slasher([1, 2, 3], 2);
+}());
