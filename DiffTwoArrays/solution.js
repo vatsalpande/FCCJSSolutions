424a41946dc6b1414370dd0b431c9e2c9ca3e2a9
Vatsal <vatspande@gmail.com>
2016-08-04 23:34:58 -0700

    Problem statements. next question to do

---
+++ DiffTwoArrays/solution.js
@@ -0,0 +1,14 @@
+(function IIFE(){
+    "use strict";
+    function diffTwoArrays(array1, array2){
+
+    }
+
+
+    diffTwoArrays()
+})();
+
+/*diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
+    ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].IIFE
+
+*/
---
+++ DiffTwoArrays/solution_linted.js
@@ -0,0 +1,10 @@
+( function() {
+    "use strict";
+    function diffArrays( array1, array2 ) {
+
+    }
+    diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+}());
+/*diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
+    ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+*/
