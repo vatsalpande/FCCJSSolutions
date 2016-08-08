24ebabb98860e5b7ea42afd65a0b4666ab6cf303
Vatsal <vatspande@gmail.com>
2016-08-08 00:47:04 -0700

    Added comments to solution in Missing Letters exercise

--- missingLetter/solution.js
+++ missingLetter/solution.js
@@ -1,6 +1,11 @@
 (function IIFE() {
     "use strict";
-
+    /**
+     * Split the string and then compare evey value with previous value.
+     * As soon as the diference is greater than 1 or 2 to be precise just subtract currentValue -1 or add 1 to lastValue
+     * @param str
+     * @returns {*}
+     */
     function fearNotLetter(str) {
         var lastValue, currentValue, missingString;
         str.split("").forEach(function(item) {
